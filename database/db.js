9bef3221039a668ab009188c86e1d0d684d40f12
manoraApp <manora.app@gmail.com>
2025-11-06 20:52:36 +0530

    Add files via upload
    ok

---
+++ database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () =>{ 
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/Ekart-YT`) 
+        console.log("MongoDB connected successfully"); 
+     }
+
+    catch(error){
+        console.log("Error in DB connection", error);
+    }
+}
+
+export default connectDB;   
